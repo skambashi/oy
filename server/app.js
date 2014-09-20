7c45cee0d565caa892c852b8f0948c261cd4138e
Seikun Kambashi <seikun_lee@hotmail.com>
2014-09-20 13:23:03 -0400

    callback cleanup

--- server/app.js
+++ server/app.js
@@ -3,141 +3,8 @@ var colors = require('colors');
 var parser = require('body-parser');
 var constants = require('./../../constants');
 var client = require('twilio')(constants.twilio_sid, constants.auth_token);
-var mongoose = require('mongoose');
 
-var db_uri = 'mongodb://localhost/oy';
-mongoose.connect(db_uri, function(err, res){
-    if (err) {
-        console.log(('[DB] Error connecting to: ' + db_uri + '. ' + err).red);
-    } else {
-        console.log(('[DB] Successfully connected to: ' + db_uri + '. ').yellow);
-    }
-});
-
-var db = mongoose.connection;
-
-var pairSchema = new mongoose.Schema({
-    alpha: {type: String, required: true},
-    omega: {type: String, required: true}
-});
-var Pair = mongoose.model('Pairs', pairSchema);
-
-var lonely = "";
-
-addPair = function(l1, l2) {
-    var pair = new Pair({
-        'alpha': l1,
-        'omega': l2
-    });
-    pair.save(function(err){
-        if (err) {
-            console.log(('[DB] Error when saving new pair: ' + err).red);
-        } else {
-            console.log('[DB] Successfully saved new pair!'.green);
-            client.messages.create({
-                body: 'You have been connected!',
-                to: l1,
-                from: constants.from_phone
-            }, function(err, message){
-                if (err) {
-                    console.log('[ERR]'.red, err.red);
-                }
-            });
-            client.messages.create({
-                body: 'You have been connected!',
-                to: l2,
-                from: constants.from_phone
-            }, function(err, message){
-                if (err) {
-                    console.log('[ERR]'.red, err.red);
-                }
-            });
-        }
-    });
-}
-
-getPairNumber = function(number, callback){
-    Pair.findOne({$or : [{alpha : number}, {omega : number}]}, function(err, pair){
-        if (err) {
-            console.log(('[DB] Error while getting pair number:' + err).red);
-	    callback(false);
-        } else if (pair) {
-            if (number == pair.alpha) {
-                 callback(pair.omega);
-            }
-            callback(pair.alpha);
-        } else {
-            console.log('[DB] No user found.'.yellow);
-            if (lonely == "") {
-                lonely = number;
-                client.messages.create({
-                    body: 'Matching...',
-                    to: number,
-                    from: constants.from_phone
-                }, function(err, message){
-                    if (err) {
-                        console.log('[ERR]'.red, err.red);
-                    }
-                });
-            } else {
-                addPair(number, lonely);
-            }
-            callback(false);
-        }
-    });
-}
-
-terminateUser = function(number) {
-    Pair.findOne({$or: [{'alpha':number},{'omega':number}]}, function(err, pair) {
-        // User not found
-        if (err) {
-	    console.log('TERMINATE USER ERR:', err);
-        // Else, returning user
-        } else if (pair) {
-	    console.log('TERMINATE USER:', number);
-            var left_over;
-	    var to_remove;
-            if (pair.alpha == number) {
-                left_over = pair.omega;
-		to_remove = pair.alpha;
-            } else {
-                left_over = pair.alpha;
-		to_remove = pair.omega;
-	    }
-            client.messages.create({
-                body: 'The other person has disconnected...\nMatching...',
-                to: left_over,
-                from: constants.from_phone
-            }, function(err, message){
-                if (err) {
-                    console.log('[ERR]'.red, err.red);
-                }
-            });
-	    client.messages.create({
-                body: 'Diconnected.',
-                to: to_remove,
-                from: constants.from_phone
-            }, function(err, message){
-                if (err) {
-                    console.log('[ERR]'.red, err.red);
-                }
-            });
-            pair.remove();
-            if (lonely == ''){
-                lonely = left_over;
-            } else {
-                addPair(lonely, left_over);
-            }
-	// Pair not found
-        } else {
-	    if (lonely == number) {
-		lonely = '';
-	    }
-	}
-    });
-}
-
-//==============================================================
+var db = require('./db');
 
 var app = express();
 var port = 80;
@@ -151,23 +18,23 @@ app.post('/sms', function(req, res){
     var body = req.body.Body;
     console.log('[SMS] From:'.green, from.yellow, 'Body:'.green, body.yellow);
     if (/^pce$/i.test(body)){
-	terminateUser(from);
+		db.terminateUser(from);
         console.log('[SMS]'.green, from.yellow, 'Stopped.'.red);
     } else {
-	console.log('getPairNumber:', from);
-        getPairNumber(from, function (result) {
-	    if (result) {
-                client.messages.create({
-	            body: body,
-	            to: result,
-	            from: constants.from_phone
-	        }, function(err, message){
-	            if (err) {
-	                console.log('[ERR]'.red, err.red);
-	            }
-	        });
-	    }
-	});
+		console.log('getPairNumber:', from);
+        db.getPairNumber(from, function (result) {
+		    if (result) {
+	            client.messages.create({
+		            body: body,
+		            to: result,
+		            from: constants.from_phone
+		        }, function(err, message){
+		            if (err) {
+		                console.log('[ERR]'.red, err.red);
+		            }
+		        });
+		    }
+		});
     }
 });
 
--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 var colors = require('colors');
-var mongoose = require('mongoose');
 var constants = require('./../../constants');
 var client = require('twilio')(constants.twilio_sid, constants.auth_token);
+var mongoose = require('mongoose');
+
 var db_uri = 'mongodb://localhost/oy';
 mongoose.connect(db_uri, function(err, res){
     if (err) {
@@ -30,7 +31,7 @@ addPair = function(l1, l2) {
         if (err) {
             console.log(('[DB] Error when saving new pair: ' + err).red);
         } else {
-            console.log('[DB] Successfully saved new pair!'.green);
+            console.log(('[DB] Successfully saved new pair ' + pair.alpha + ', ' + pair.omega).green);
             client.messages.create({
                 body: 'You have been connected!',
                 to: l1,
@@ -53,15 +54,16 @@ addPair = function(l1, l2) {
     });
 }
 
-exports.getPairNumber = function(number){
-    User.findOne({$or : [{alpha : number}, {omega : number}]}, function(err, pair){
+exports.getPairNumber = function(number, callback){
+    Pair.findOne({$or : [{alpha : number}, {omega : number}]}, function(err, pair){
         if (err) {
             console.log(('[DB] Error while getting pair number:' + err).red);
+            callback(false);
         } else if (pair) {
             if (number == pair.alpha) {
-                return pair.omega;
+                 callback(pair.omega);
             }
-            return pair.alpha;
+            callback(pair.alpha);
         } else {
             console.log('[DB] No user found.'.yellow);
             if (lonely == "") {
@@ -78,7 +80,7 @@ exports.getPairNumber = function(number){
             } else {
                 addPair(number, lonely);
             }
-            return false;
+            callback(false);
         }
     });
 }
@@ -87,19 +89,19 @@ exports.terminateUser = function(number) {
     Pair.findOne({$or: [{'alpha':number},{'omega':number}]}, function(err, pair) {
         // User not found
         if (err) {
-	    console.log('TERMINATE USER ERR:', err);
+            console.log(('[DB] Error while getting user to terminate: ' + err).red);
         // Else, returning user
         } else if (pair) {
-	    console.log('TERMINATE USER:', number);
+            console.log(('[DB] Terminate: ' + number).yellow);
             var left_over;
-	    var to_remove;
+            var to_remove;
             if (pair.alpha == number) {
                 left_over = pair.omega;
-		to_remove = pair.alpha;
+                to_remove = pair.alpha;
             } else {
                 left_over = pair.alpha;
-		to_remove = pair.omega;
-	    }
+                to_remove = pair.omega;
+            }
             client.messages.create({
                 body: 'The other person has disconnected...\nMatching...',
                 to: left_over,
@@ -109,7 +111,7 @@ exports.terminateUser = function(number) {
                     console.log('[ERR]'.red, err.red);
                 }
             });
-	    client.messages.create({
+            client.messages.create({
                 body: 'Diconnected.',
                 to: to_remove,
                 from: constants.from_phone
@@ -118,17 +120,24 @@ exports.terminateUser = function(number) {
                     console.log('[ERR]'.red, err.red);
                 }
             });
-            pair.remove();
             if (lonely == ''){
                 lonely = left_over;
             } else {
                 addPair(lonely, left_over);
             }
-	// Pair not found
+        // Pair not found
         } else {
-	    if (lonely == number) {
-		lonely = '';
-	    }
-	}
+            if (lonely == number) {
+                client.messages.create({
+                    body: 'Diconnected.',
+                    to: to_remove,
+                    from: constants.from_phone
+                }, function(err, message){
+                    if (err) {
+                        console.log('[ERR]'.red, err.red);
+                    }
+                });
+            }
+        }
     });
 }
